4ecce7653dd28d2fcb9f709dfe649f6859418ea9
Andy-Robertson <andrew.j.robertson@posteo.uk>
2020-07-19 17:09:43 +0100

    Major rewrite, all basic function added

--- script.js
+++ script.js
@@ -1,30 +1,60 @@
 const mainGridContainer = document.querySelector('#mainGridContainer');
-const square = document.createElement('div');
+const resetGrid = document.querySelector('#resetGrid');
+const randomColour = document.querySelector('#randomColour')
+const grayScale = document.querySelector('#grayScale');
 
-//function to appends 256 squares to document
-function buildGrid() {
 
-    for (i = 0; i < 256; i++) {
 
-        const square = document.createElement('div');
-        square.classList.add('square');
-        mainGridContainer.appendChild(square);
-        square.addEventListener('mouseover', alertFunction);
+
+
+
+let resetFunction = () => {
+
+    let mainGridContainer = document.getElementById('mainGridContainer');
+    while (mainGridContainer.firstChild) {
+        mainGridContainer.removeChild(mainGridContainer.firstChild);
     }
-}
-buildGrid();
 
-//changes background colour of class square, called by function buildGrid
-function alertFunction(square) {
+    gridSideLength = prompt("How many cells per side?", 16)
+    buildGridFunction(numberOfCells = (gridSideLength * gridSideLength));
 
-    square.target.style.background = 'blue';
+    const cellVariable = '--grid-sides';
+    document.documentElement.style.setProperty(cellVariable, gridSideLength);
 
-}
+};
+resetGrid.addEventListener('click', resetFunction);
 
 
-//resets grid prompts for grid size
-function reset(square) {
 
-    square.target.style.background = 'white';
 
-}
+let colourFunction = () => {
+
+};
+randomColour.addEventListener('click', colourFunction);
+
+let grayscaleFunction = () => {
+    alert("grayscale");
+};
+grayScale.addEventListener('click', grayscaleFunction);
+
+let buildGridFunction = () => {
+
+    for (let i = 0; i < numberOfCells; i++) {
+
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.style.backgroundColor = 'lightblue';
+        mainGridContainer.appendChild(cell);
+        cell.addEventListener('mouseover', cellHover)
+
+    }
+
+    console.log(gridSideLength);
+    console.log(numberOfCells);
+
+};
+
+let cellHover = (cell) => {
+
+    cell.target.style.backgroundColor = 'RED';
+};
